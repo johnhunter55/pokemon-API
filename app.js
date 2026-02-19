a01494e75cd666bfc78d8e7bfc2dc1d3329d1efd
John Hunter <johnroberthunter1@gmail.com>
2026-02-19 08:52:09 -0700

    cllcking on the name now brings more info to look at

--- app.js
+++ app.js
@@ -1,23 +1,45 @@
 async function fetchPokemon() {
-  const res = await fetch("https://pokeapi.co/api/v2/pokemon?");
+  const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=200");
   const data = await res.json();
 
   let index = 0;
+  let currentPokemonData = null;
 
   const updateUI = async () => {
     const pokemonRes = await fetch(data.results[index].url);
-    const pokemonData = await pokemonRes.json();
-    document.getElementById("pokemon-image").src =
-      pokemonData.sprites.front_default;
+    currentPokemonData = await pokemonRes.json();
 
-    document.getElementById("pokemon-info").innerText =
-      `Name: ${pokemonData.name}\nHeight: ${pokemonData.height}\nWeight: ${pokemonData.weight}`;
+    const img = document.getElementById("pokemon-image");
+    await new Promise((resolve) => {
+      img.onload = resolve;
+      img.onerror = resolve;
+      img.src = currentPokemonData.sprites.front_default;
+    });
+
+    document.getElementById("pokemon-info").innerHTML =
+      `<span id="pokemon-name" style="font-weight: bold; text-transform: capitalize; cursor: pointer;">Name: ${currentPokemonData.name}</span><br>Height: ${currentPokemonData.height}<br>Weight: ${currentPokemonData.weight}`;
   };
 
   await updateUI();
 
   const lB = document.getElementById("left");
   const rB = document.getElementById("right");
+  const pokInfo = document.getElementById("pokemon-info");
+
+  pokInfo.addEventListener("click", async () => {
+    const res = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${currentPokemonData.name}`,
+    );
+    const details = await res.json();
+
+    pokInfo.innerHTML = `<span id="pokemon-name" style="font-weight: bold; text-transform: capitalize;">Name: ${details.name}</span><br>Height: ${details.height}<br>Weight: ${details.weight}<br>Base XP: ${details.base_experience}<br>Types: ${details.types
+      .map((t) => t.type.name)
+      .join(", ")}<br>Abilities: ${details.abilities
+      .map((a) => a.ability.name)
+      .join(", ")}<br>Stats: ${details.stats
+      .map((s) => `${s.stat.name}: ${s.base_stat}`)
+      .join(", ")}`;
+  });
 
   lB.addEventListener("click", async () => {
     if (index > 0) {
@@ -38,12 +60,6 @@ async function fetchPokemon() {
       await updateUI();
     }
   });
-
-  //   for (let i = 0; i < data.results.length; i++) {
-  //     const re2 = await fetch(data.results[i].url);
-  //     const data2 = await re2.json();
-  //     console.log(data2);
-  //   }
 }
 
 fetchPokemon();
